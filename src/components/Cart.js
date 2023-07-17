ba9c994abbca61827f55fe473e8d89fcf0e8be2b
bubonka4444 <buneev.18.03@gmail.com>
2023-07-18 00:42:03 +0400

    cart pseudo-fix

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,15 +2,18 @@ import React from "react";
 import CartItem from "./CartItem";
 import { TbMoodCry } from 'react-icons/tb'
 
+
 function Cart ({cartItems, removeFromCart}) {
     
+    console.log(cartItems)
+    console.log(cartItems.length)
     return (
         <main className="cart">
             {cartItems.length > 0 ? <h1 className="cart-title">Корзина</h1> 
             : <h1 className="cart-title">Корзина пуста < TbMoodCry className="sad"/> </h1>}
             <section className="cartItems">
-                {cartItems?.map((item) => (
-                <CartItem item={item}  key={item.title} removeFromCart={removeFromCart} />
+                {cartItems?.map((cartItem) => (
+                <CartItem cartItem={cartItem}  key={Math.random()} removeFromCart={removeFromCart} />
                 ))}
             </section>
             
